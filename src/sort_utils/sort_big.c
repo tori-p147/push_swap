76a56ca337e79e2bfbc9473c11aa08ab71657935
Viktoriia Matsuda <vmatsuda@c4r2s8.42tokyo.jp>
2025-08-10 15:10:20 +0900

    add

--- src/sort_utils/sort_big.c
+++ src/sort_utils/sort_big.c
@@ -6,7 +6,7 @@
 /*   By: vmatsuda <vmatsuda@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/04 15:22:36 by vmatsuda          #+#    #+#             */
-/*   Updated: 2025/08/09 21:43:16 by vmatsuda         ###   ########.fr       */
+/*   Updated: 2025/08/09 21:59:14 by vmatsuda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,8 @@ void	process_a(t_all *all)
 	int		cmd_i;
 	int		size;
 	char	**ptr_arr;
+	t_node	*ptr_head_a;
+	t_node	*ptr_head_b;
 
 	cmd_i = 0;
 	all->mid = calculate_mid_order(all->stack_a, all->flag);
@@ -60,15 +62,28 @@ void	process_a(t_all *all)
 			str_arr[cmd_i] = ft_strdup(rotate(stack, 'a'));
 		check_strdup(all, str_arr, cmd_i);
 		cmd_i++;
-	}
-	str_arr[cmd_i] = NULL;
-	ptr_arr = str_arr;
-	while (*str_arr)
-	{
-		ft_printf("%s", *str_arr);
-		str_arr++;
-	}
-	free_strs(ptr_arr);
+		while (ptr_head_a)
+		{
+			ft_printf("stack a after proess b[%d] = %d flag = %d\n",
+				ptr_head_a->order, ptr_head_a->value, ptr_head_a->flag);
+			ptr_head_a = ptr_head_a->next;
+		}
+		ptr_head_b = all->stack_b->head;
+		while (ptr_head_b)
+		{
+			ft_printf("stack b after proess b[%d] = %d flag = %d\n",
+				ptr_head_b->order, ptr_head_b->value, ptr_head_b->flag);
+			ptr_head_b = ptr_head_b->next;
+		}
+}
+str_arr[cmd_i] = NULL;
+ptr_arr = str_arr;
+while (*str_arr)
+{
+	ft_printf("%s", *str_arr);
+	str_arr++;
+}
+free_strs(ptr_arr);
 }
 
 int	count_flag_elements(t_llist *stack, int flag)
@@ -126,11 +141,13 @@ void	process_b(t_all *all)
 {
 	t_node	*curr;
 	int		cmd_i;
-	// char	**str_arr;
-	// char	**ptr_arr;
 	int		flag_nodes;
 	int		current_flag;
+	t_node	*ptr_head_a;
+	t_node	*ptr_head_b;
 
+	// char	**str_arr;
+	// char	**ptr_arr;
 	current_flag = all->flag;
 	// ft_printf("process b start\n");
 	all->mid = calculate_mid_order(all->stack_b, current_flag);
@@ -148,16 +165,18 @@ void	process_b(t_all *all)
 	{
 		// ft_printf("count flag nodes %d in while loop\n", flag_nodes);
 		curr = all->stack_b->head;
-		// ft_printf("NEXT = [%d] MID = %d all.flag = %d curr = [%d] flag =%d next = %p\n", all->next, all->mid, all->flag, curr->order,
+		// ft_printf("NEXT = [%d] MID = %d all.flag =
+			//%d curr = [%d] flag =%d next = %p\n", all->next, all->mid,
+			//all->flag, curr->order,
 		// 	curr->flag, curr->next);
 		if (curr->order == all->next)
 		{
 			if (curr->flag == current_flag)
 				flag_nodes--;
-			ft_printf("%s",push_a(all));
+			ft_printf("%s", push_a(all));
 			// check_strdup(all, str_arr, cmd_i);
 			cmd_i++;
-			ft_printf("%s",rotate(all->stack_a, 'a'));
+			ft_printf("%s", rotate(all->stack_a, 'a'));
 			// check_strdup(all, str_arr, cmd_i);
 			all->next++;
 			cmd_i++;
@@ -167,7 +186,7 @@ void	process_b(t_all *all)
 		{
 			if (curr->order >= all->mid)
 			{
-				ft_printf("%s",push_a(all));
+				ft_printf("%s", push_a(all));
 				// check_strdup(all, str_arr, cmd_i);
 				all->stack_a->head->flag = all->flag + 1;
 				flag_nodes--;
@@ -175,7 +194,7 @@ void	process_b(t_all *all)
 			}
 			else
 			{
-				ft_printf("%s",rotate(all->stack_b, 'b'));
+				ft_printf("%s", rotate(all->stack_b, 'b'));
 				// check_strdup(all, str_arr, cmd_i);
 				cmd_i++;
 			}
@@ -214,45 +233,52 @@ int	stack_contains_flag(t_llist *stack, int flag)
 // ./push_swap 9 6 3 13 7 11 2 15 1 10 5 12 4 14 8
 void	sort_big(t_all *all)
 {
-	// t_node	*ptr_head_a;
-	// t_node	*ptr_head_b;
-	// int i;
+	t_node	*ptr_head_a;
+	t_node	*ptr_head_b;
 
+	// int i;
 	// i = 0;
-	while (all->stack_b->size != 0 || stack_contains_flag(all->stack_a, all->flag))
+	while (all->stack_b->size != 0 || stack_contains_flag(all->stack_a,
+			all->flag))
 	{
 		// ft_printf("current next = %d, flag = %d\n", all->next, all->flag);
 		process_a(all);
 		// ptr_head_a = all->stack_a->head;
-	// 	while (ptr_head_a)
-	// {
-	// 	ft_printf("stack a before proess b [%d] = %d flag = %d\n", ptr_head_a->order, ptr_head_a->value, ptr_head_a->flag);
-	// 	ptr_head_a = ptr_head_a->next;
-	// }
-	// ptr_head_b = all->stack_b->head;
-	// while (ptr_head_b)
-	// {
-	// 	ft_printf("stack b after proess b [%d] = %d flag = %d\n", ptr_head_b->order, ptr_head_b->value, ptr_head_b->flag);
-	// 	ptr_head_b = ptr_head_b->next;
-	// }
-	// 	ft_printf("current next = %d, flag = %d\n", all->next, all->flag);
+		// 	while (ptr_head_a)
+		// {
+		// 	ft_printf("stack a before proess b [%d] = %d flag = %d\n",
+				ptr_head_a->order, ptr_head_a->value, ptr_head_a->flag);
+		// 	ptr_head_a = ptr_head_a->next;
+		// }
+		// ptr_head_b = all->stack_b->head;
+		// while (ptr_head_b)
+		// {
+		// 	ft_printf("stack b after proess b [%d] = %d flag = %d\n",
+				ptr_head_b->order, ptr_head_b->value, ptr_head_b->flag);
+		// 	ptr_head_b = ptr_head_b->next;
+		// }
+		// 	ft_printf("current next = %d, flag = %d\n", all->next, all->flag);
 		process_b(all);
-		if (!stack_contains_flag(all->stack_b, all->flag) && !stack_contains_flag(all->stack_a, all->flag)) 
-        	all->flag++;
-		// ptr_head_a = all->stack_a->head;
+		if (!stack_contains_flag(all->stack_b, all->flag)
+			&& !stack_contains_flag(all->stack_a, all->flag))
+			all->flag++;
+		ptr_head_a = all->stack_a->head;
 		// i++;
-	// 	ft_printf("loop %d result = %d, flag = %d\n", i, all->next, all->flag);
-	// while (ptr_head_a)
-	// {
-	// 	ft_printf("stack a after proess b[%d] = %d flag = %d\n", ptr_head_a->order, ptr_head_a->value, ptr_head_a->flag);
-	// 	ptr_head_a = ptr_head_a->next;
-	// }
-	// ptr_head_b = all->stack_b->head;
-	// while (ptr_head_b)
-	// {
-	// 	ft_printf("stack b after proess b[%d] = %d flag = %d\n", ptr_head_b->order, ptr_head_b->value, ptr_head_b->flag);
-	// 	ptr_head_b = ptr_head_b->next;
-	// }
-	// }
+		// 	ft_printf("loop %d result = %d, flag = %d\n", i, all->next,
+				all->flag);
+		// while (ptr_head_a)
+		// {
+		// 	ft_printf("stack a after proess b[%d] = %d flag = %d\n",
+				ptr_head_a->order, ptr_head_a->value, ptr_head_a->flag);
+		// 	ptr_head_a = ptr_head_a->next;
+		// }
+		// ptr_head_b = all->stack_b->head;
+		// while (ptr_head_b)
+		// {
+		// 	ft_printf("stack b after proess b[%d] = %d flag = %d\n",
+				ptr_head_b->order, ptr_head_b->value, ptr_head_b->flag);
+		// 	ptr_head_b = ptr_head_b->next;
+		// }
+		// }
 	}
 }
